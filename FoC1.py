7e7b1a1d0336bb71ce357f51a9680594089762ca
Astaroth <astaroth@localhost.localdomain>
2020-06-15 20:56:40 +0500

    rsa function started

--- FoC1.py
+++ FoC1.py
@@ -1,22 +1,33 @@
-def func1():
+def prime_nos():
     sum = 0
     value = input ("Enter name\n")
     for x in value :
         sum = sum + ord(x)
-    bum = sum
+    prime = sum
     i = 0
     while i<2:
-        bum=bum+1
-        for n in range (2,bum):
-            if (bum % n) == 0 :
+        prime = prime + 1
+        for n in range (2,prime):
+            if (prime % n) == 0 :
                 break
         else :
             i = i+1
             if i == 1 :
-                p=bum
+                p = prime
             elif i == 2 :
-                q = bum
-    return p,q;
+                q = prime
+    return p,q     
 
 
+def rsa(p, q):
+    n = p * q
+    phy = (p-1) * (q-1)
+    i = 0
+    while i != phy:
+        
+
+
+
+
+rsa(p,q)
 print(func1())
