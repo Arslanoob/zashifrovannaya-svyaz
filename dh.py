ea42d79ba78aeffcb5ab72504cc58924acd6c529
Arslanoob <marslanaslam7860@gmail.com>
2020-06-17 14:49:54 +0500

    cleaning code

--- dh.py
+++
@@ -1,7 +0,0 @@
-def dh(x_a,x_s,a,q):
-    pub_A = (a**x_a)%q #here we create the pub key which we need to send to saad. we at arslan side rn
-    pub_S = (a**x_s)%q# here we receive saads pub key . which we use in next line to deduce secret key
-    k = (pub_S**x_a)%q
-    return k
-
-print(dh(165893,108037,3,17))
--- test.py
+++
@@ -1,8 +0,0 @@
-def __init__(public_key1, public_key2, private_key):
-        public_key1 = public_key1
-        public_key2 = public_key2
-        private_key = private_key
-        full_key = None
-        
-__init__(176399,416021,108037)
-print(public_key1)
